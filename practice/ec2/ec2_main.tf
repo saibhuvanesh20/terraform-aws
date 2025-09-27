23a82e3d53618d03520548857bee64e48578646d
sai bhuvanesh <saibhuvanesh@sais-MacBook-Air.local>
2025-09-27 14:56:59 +0530

    corrected security groups resource

--- practice/ec2/ec2_main.tf
+++ practice/ec2/ec2_main.tf
@@ -1,6 +1,7 @@
 resource "aws_instance" "ec2-practice" {
-  instance_type = "t2.small"
-  security_groups = [ aws_security_group.allow_tls.id ]
+  ami             = "ami-09c813fb71547fc4f"
+  instance_type   = "t2.small"
+  security_groups = [aws_security_group.allow_tls.id]
 
   tags = {
     Name = "ec2-practice"
@@ -9,19 +10,19 @@ resource "aws_instance" "ec2-practice" {
 
 #SECURITY GROUPS
 resource "aws_security_group" "allow_tls" {
-  
-  ingress = {
-    from_port = 22
-    to_port = 22
-    protocol = "tcp"
-    cidr_block  = ["0.0.0.0/0"]
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
-  egress = {
-    from_port = 0
-    to_port = 0
-    protocol = "-1"
-    cidr_block = ["0.0.0.0/0"]
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   tags = {
